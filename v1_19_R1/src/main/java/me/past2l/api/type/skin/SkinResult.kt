2292205e25f59002052a3677c246048e25ef7162
Dohyeon Yun <33440293+Past2l@users.noreply.github.com>
2023-03-04 20:28:56 +0900

    add: `type` package

--- v1_19_R1/src/main/java/me/past2l/api/type/skin/SkinResult.kt
+++
@@ -1,9 +0,0 @@
-package me.past2l.api.type.skin
-
-data class SkinResult(
-    val texture: String = "",
-    val signature: String = "",
-    val skin: String = "",
-    val cape: String = "",
-)
-
